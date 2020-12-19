4d4d64ab17500e2ceecab3909341276995853f18
Bonit0Flakes <19uca004@gmail.com>
2020-12-19 11:32:07 +0000

    dark mode toggler still need to implement local storage for it

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector("#dark");
+const bg_img = document.querySelector('.bg_img');
 // Listen for a click on the button
 btn.addEventListener("click", function() {
   // Then toggle (add/remove) the .dark-theme class to the body
   console.log('clicked the btn');
   document.body.classList.toggle("dark-theme");
+  bg_img.classList.toggle("hidden");
 });
 
 alert
