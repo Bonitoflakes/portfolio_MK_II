9a9d32eefd53651013358d5d1701d739134e4578
Bonit0Flakes <19uca004@gmail.com>
2020-12-19 11:35:15 +0000

    update: dark mode using local storage

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const btn = document.querySelector("#dark");
-const bg_img = document.querySelector('.bg_img');
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  console.log('clicked the btn');
-  document.body.classList.toggle("dark-theme");
-  bg_img.classList.toggle("hidden");
+// const bg_img = document.querySelector('.bg_img');
+// // Listen for a click on the button
+// btn.addEventListener("click", function() {
+//   // Then toggle (add/remove) the .dark-theme class to the body
+//   console.log('clicked the btn');
+//   document.body.classList.toggle("dark-theme");
+//   bg_img.classList.toggle("hidden");
+// });
+
+btn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark-theme' : 'light');
 });
 
-alert
+if (localStorage.getItem('theme') === 'dark-theme') {
+    document.body.classList.add('dark-theme');
+}
