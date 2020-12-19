53e28d6bd4a8911d9f9bb91637f630279e2cf68f
Bonitoflakes <75728765+Bonitoflakes@users.noreply.github.com>
2020-12-19 15:00:40 +0530

    dark mode

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-const btn = document.querySelector(".btn-toggle");
+const btn = document.querySelector("#dark");
 // Listen for a click on the button
 btn.addEventListener("click", function() {
-    getComputedStyle(document.documentElement)
-    .getPropertyValue('--bg-color');
-    getComputedStyle(document.documentElement)
-    .getPropertyValue('--color');
-    document.documentElement.style
-    .setProperty('--bg-color', 'pink');
-    document.documentElement.style
-    .setProperty('--color', 'pink');
+  // Then toggle (add/remove) the .dark-theme class to the body
+  console.log('clicked the btn');
+  document.body.classList.toggle("dark-theme");
 });
 
-
+alert
